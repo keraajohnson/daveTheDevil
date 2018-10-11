fa911148c47aae2737e8932a25e40997488cb595
Kera <kera.andrea@gmail.com>
2018-10-11 16:44:22 -0400

    Major upates to website

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,3 +10,17 @@ const mySwiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     }
 })
+
+$(document).ready(function () {
+    $('.fullMenu').on('click', function () {
+        $('.menu').fadeIn("slow");
+    });
+    $('.exit').on('click', function () {
+        $('.menu').fadeOut("slow");
+    });
+    $('.navLink__mobile').on('click', function () {
+        $('.menu').fadeOut("slow");
+    }); 
+});
+
+   
